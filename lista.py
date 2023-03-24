f236913b23941a399ec04877db38ee625f40620d
edilson gomes de lima <edskateicevida@gmail.com>
2023-03-24 16:14:35 -0300

    função float

--- lista.py
+++
@@ -1,7 +0,0 @@
-numeros = [2, 4, 6, 8, 10]
-
-produto = 1
-
-for num in numeros: 
-    produto *= num
-    print(produto)
---
+++ soma.py
@@ -0,0 +1,8 @@
+num1 = float(input('Digite i primeiro numero:'))
+num2 = float(input('Digite o segundo numero:'))
+
+soma = num1 + num2
+
+print('A soma de ', num1, "é", num2, "é", soma)
+
+
