ddfbc5aaa842a56c8b9fc177826806f8433a0a0a
edilson gomes de lima <edskateicevida@gmail.com>
2023-03-24 14:58:10 -0300

    media

---
+++ media.py
@@ -0,0 +1,9 @@
+def calcular_media(numeros):
+    total = sum(numeros)
+    quantidade = len(numeros)
+    media = total / quantidade
+    return media
+
+lista = [2, 4, 6, 8, 10]
+media = calcular_media(lista)
+print(media)
