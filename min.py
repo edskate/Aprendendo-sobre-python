7fa30b152aaba2716b129800d420ed54ea84bd5d
edilson gomes de lima <edskateicevida@gmail.com>
2023-03-24 14:53:25 -0300

    soma

---
+++ min.py
@@ -0,0 +1,11 @@
+# Este é um comentário em Python
+
+# Imprimir uma mensagem na tela
+print("Olá, mundo!")
+
+# Definir uma variável
+mensagem = "Estou aprendendo Python!"
+
+# Imprimir o valor da variável
+print(mensagem)
+
